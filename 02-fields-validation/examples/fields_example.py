c7d93a486a2d60b89333f79e68450e19f8cf2559
yash842004 <yashbhimani842004@gmail.com>
2026-03-11 22:16:41 +0530

    update in fields and validations 2

---
+++ 02-fields-validation/examples/fields_example.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import List,dict,Optional
+
+
+class Card(BaseModel):
+    user_id: int
+    items: List[str]
+    queataty:dist[str, int]
+
+
+class BlogPost(BaseModel):
+    title: str
+    content: str
+    image_url: Optional[str] = None
+
+    
+
+
